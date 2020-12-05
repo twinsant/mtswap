e3d42df4c856f4f8857e26bc2409609dc3492caa
twinsant <twinsant@gmail.com>
2020-12-05 16:26:45 +0800

    Fix: lost changes.

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,65 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+//import { ChainId, Token, Fetcher } from '@uniswap/sdk'
+import { ethers } from 'ethers'
 
-export default function Home() {
+
+export default function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Hello, Uniswap</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          { props.blocks }
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p>{ props.name }</p>
+        <p>{ props.symbol }</p>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  // const chainId = ChainId.MAINNET
+  const tokenAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+
+  // const DAI = await Fetcher.fetchTokenData(chainId, tokenAddress)
+
+  var network = '未知'
+  // if (chainId === 1) {
+  //   network = '主网'
+  // }else{
+  //   network = '测试网络'
+  // }
+
+  const provider = new ethers.providers.getDefaultProvider('http://localhost:8545')
+  const blocks = await provider.getBlockNumber()
+
+  const address = '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9'
+  const ABI = [
+    'function name() view returns (string)',
+    'function symbol() view returns (string)',
+
+    'function balanceOf(address) view returns (uint)',
+
+    'function transfer(address to, uint amount)',
+
+    'event Transfer(address indexed from, address indexed to, uint amount)',
+  ]
+  const contract = new ethers.Contract(address, ABI, provider)
+  const name = await contract.name()
+  const symbol = await contract.symbol()
+
+  return {
+    props: {
+      network,
+      blocks,
+      name,
+      symbol
+    }
+  }
 }
