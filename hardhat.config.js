ed4c572923d787505a51872d4953d8525c261e4e
twinsant <twinsant@gmail.com>
2020-12-11 11:48:37 +0800

    Add: ropsten test net config.

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,14 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require('@nomiclabs/hardhat-ethers');
+const { infuraApiKey, mnemonic } = require('./scripts/secrets.json')
 
 module.exports = {
   solidity: "0.6.12",
+  networks: {
+    ropsten: {
+      url: 'https://ropsten.infura.io/v3/projectid',
+      accounts: {mnemonic: mnemonic}
+    }
+  }
 };
