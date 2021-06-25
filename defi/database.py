47b96ade0e016f8131387283ff561a9b839c4a3d
twinsant <twinsant@gmail.com>
2021-06-25 15:07:23 +0800

    Add: MongoDB.

---
+++ defi/database.py
@@ -0,0 +1,23 @@
+# database.py
+import pprint
+
+from pymongo import MongoClient
+from pymongo import ASCENDING
+
+class MongoDB:
+    def __init__(self):
+        self.client = MongoClient()
+        self.db = self.client.uniswap
+
+    def getSyncedPairs(self):
+        max_idx = 0
+        pairs = self.db.pairs
+        doc = pairs.find_one(sort=[('idx', -1)])
+        if doc:
+            max_idx = doc['idx']
+        return max_idx
+
+    def save(self, doc):
+        pairs = self.db.pairs
+        pairs.create_index([('idx', ASCENDING)], unique=True)
+        pairs.insert_one(doc)
--- defi/uniswap.py
+++ defi/uniswap.py
@@ -1,4 +1,6 @@
 from helper import DeFiContract
+from database import MongoDB
+import web3
 
 
 UNISWAP_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
@@ -7,23 +9,55 @@ factory_contract = DeFiContract(UNISWAP_ADDRESS, 'Factory')
 
 #returns a count of all the trading pairs on uniswap
 allPairsLength = factory_contract.allPairsLength()
-print(allPairsLength)
 
-for i in range(1, 10):
+db = MongoDB()
+syncedPairs = db.getSyncedPairs()
+
+for i in range(syncedPairs + 1, allPairsLength):
     allPairs_address = factory_contract.allPairs(i)
     pair_contract = DeFiContract(allPairs_address, 'Pair')
 
     token0_address = pair_contract.token0()
     token0 = DeFiContract(token0_address, 'ERC20')
-    symbol0 = token0.symbol()
-    decimals0 = token0.decimals()
+    try:
+        symbol0 = token0.symbol()
+    except OverflowError:
+        symbol0 = 'ERROR'
+    except web3.exceptions.ContractLogicError:
+        symbol0 = 'ERROR'
+    except web3.exceptions.BadFunctionCallOutput:
+        symbol0 = 'ERROR'
+
+    try:
+        decimals0 = token0.decimals()
+    except web3.exceptions.ContractLogicError:
+        decimals0 = 18
+    except web3.exceptions.BadFunctionCallOutput:
+        decimals0 = 18
 
     token1_address = pair_contract.token1()
     token1 = DeFiContract(token1_address, 'ERC20')
-    symbol1 = token1.symbol()
-    decimals1 = token1.decimals()
+    try:
+        symbol1 = token1.symbol()
+    except OverflowError:
+        symbol1 = 'ERROR'
+    except web3.exceptions.ContractLogicError:
+        symbol1 = 'ERROR'
+    except web3.exceptions.BadFunctionCallOutput:
+        symbol1 = 'ERROR'
+    try:
+        decimals1 = token1.decimals()
+    except web3.exceptions.BadFunctionCallOutput:
+        decimals1 = 18
 
     reserve0, reserve1, _ = pair_contract.getReserves()
     reserve0 = reserve0 / (10**decimals0)
     reserve1 = reserve1 / (10**decimals1)
-    print('{} {:<5} {:<5} {:>18.2f} {:>18.2f}'.format(allPairs_address, symbol0, symbol1, reserve0, reserve1))
+    print('{} {} {:<5} {:<5} {:>18.2f} {:>18.2f}'.format(i, allPairs_address, symbol0, symbol1, reserve0, reserve1))
+    doc = {
+        'idx': i,
+        'address': allPairs_address,
+        'token0': token0_address,
+        'token1': token1_address,
+    }
+    db.save(doc)
