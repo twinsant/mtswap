de791ea986475cb69bc06cef3a7945b4013b2eeb
twinsant <twinsant@gmail.com>
2021-07-02 18:58:48 +0800

    Add: token funcs.

--- defi/database.py
+++ defi/database.py
@@ -14,6 +14,29 @@ class MongoDB:
         docs = pairs.find()
         return [doc['address'] for doc in docs]
 
+    def get_all_tokens(self):
+        tokens = []
+        pairs = self.db.pairs
+        docs = pairs.find()
+        for doc in docs:
+            if not doc['token0'] in tokens:
+                tokens.append(doc['token0'])
+            if not doc['token1'] in tokens:
+                tokens.append(doc['token1'])
+        return tokens
+
+    def get_token(self, address):
+        tokens = self.db.tokens
+        tokens.create_index([('address', ASCENDING)], unique=True)
+        doc = tokens.find_one({'address': address})
+        return doc
+
+    def save_token(self, doc):
+        tokens = self.db.tokens
+        tokens.create_index([('address', ASCENDING)], unique=True)
+        tokens.insert_one(doc)
+
+
     def getSyncedPairs(self):
         max_idx = 0
         pairs = self.db.pairs
