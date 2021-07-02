0099426e82e4cfe7b9f7deee69e855430271738f
twinsant <twinsant@gmail.com>
2021-07-02 18:01:22 +0800

    Add: token tool.

---
+++ defi/token_tool.py
@@ -0,0 +1,37 @@
+from database import MongoDB
+from helper import DeFiContract
+
+def normalize_text(str):
+    return w3.toText(str) if str.find("0x") > -1 else str
+
+if __name__ == '__main__':
+    db = MongoDB()
+    tokens = db.get_all_tokens()
+    print(len(tokens))
+    for token in tokens:
+        if db.get_token(token) is None:
+            try:
+                t = DeFiContract(token, 'ERC20')
+                name = t.name()
+                symbol = t.symbol()
+                decimals = t.decimals()
+                print('{} {}({}) {}'.format(token, name, symbol, decimals))
+                db.save_token({
+                    'address': token,
+                    'name': name,
+                    'symbol': symbol,
+                    'decimals': decimals,
+                })
+            except OverflowError:
+                # https://www.gitmemory.com/issue/blockchain-etl/ethereum-etl/240/796883933
+                t = DeFiContract(token, 'ERC20Fixed')
+                name = t.name().decode('utf-8')
+                symbol = t.symbol().decode('utf-8')
+                decimals = t.decimals()
+                print('{} {}({}) {}'.format(token, name, symbol, decimals))
+                db.save_token({
+                    'address': token,
+                    'name': name,
+                    'symbol': symbol,
+                    'decimals': decimals,
+                })
